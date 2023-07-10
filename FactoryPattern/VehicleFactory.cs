87c881c206ed3c5c660e183b43892c5f472bc7e9
Eric Caro <c.aroericm@gmail.com>
2023-07-10 17:43:58 -0500

    Exercise1

---
+++ FactoryPattern/VehicleFactory.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryPattern
+{
+    public static class VehicleFactory
+    {
+        public static IVehicle GetVehicle(int wheelCount) 
+        { 
+            switch (wheelCount) 
+            {
+                case 2:
+                    return new Motorcycle();
+                case 4: 
+                    return new Car();
+                case 16: 
+                    return new Truck();
+                default:
+                    return new Car();
+    
+
+            }
+        }
+
+
+    }
+}
